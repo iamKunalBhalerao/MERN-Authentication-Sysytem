7a0ab4c976f321e08a15bd086023757db7941226
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2025-05-22 15:43:24 +0530

    Finalized send verification mail feature

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -76,10 +76,20 @@ const signin = async (req, res, next) => {
       from: process.env.EMAIL_USER,
       to: user?.email,
       subject: "Welcome to MERN Auth System",
-      text: `Welcome to MERN Authentication System, Your Account is Created with Username : ${user?.username} Email : ${user?.email}`,
-      html: `<b>Please Verify the Email with OTP : ${verificationOtp}. By Clicking </b><a href="${verificationLink}">Verify</a>`,
+      html: `<p>Welcome to MERN Authentication System, Your Account is Created with Username : ${user?.username} Email : ${user?.email}</p>
+    <br>
+      <section
+        style="width: 100%; height: 100%; padding: 2rem; display: flex; flex-direction: column; align-items: center; justify-content: center; background-color: rgb(255, 148, 72);">
+        <h2>Please Verify the Email with OTP ${verificationOtp}</h2>
+        <h1>
+            By Clicking
+            <button
+                style="background: linear-gradient(90deg, #ff6a00, #ee0979); color: #fff; border: none; padding: 12px 28px; border-radius: 30px; font-size: 1.2em; cursor: pointer; box-shadow: 0 4px 14px rgba(238,9,121,0.2); transition: background 0.3s;">
+                <a href="${verificationLink}" style="color: #fff; text-decoration: none;">Verify</a>
+            </button>
+        </h1>
+    </section>`,
     };
-
 
     const transporter = await createTransporter();
 
@@ -92,7 +102,6 @@ const signin = async (req, res, next) => {
 
     const info = await transporter.sendMail(mailOptions);
     console.log("Email sent: %s", info.messageId);
-
 
     res.status(200).json({
       error: false,
