ebdf5664c002baae9bd14e5ff7c4c9b86da94a0c
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2025-06-20 15:03:01 +0530

    frontend setup for project

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <main>
+        <h1 className="text-4xl font-black text-blue-950 ">
+          This is Main Content
+        </h1>
+      </main>
+    </>
+  );
+};
+
+export default Home;
